7ada7909850aa70029a5f8590652398d0128bd35
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-03-04 17:31:23 -0500

    added complaint routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 //User routes
-router.get('/', userController.index)
+router.get('/users', userController.index)
+router.get('/users/new', userController.new)
+router.post('/users', userController.create)
+router.get('/users/:userId', userController.show)
+router.get('/users/:userId/edit', userController.edit)
+router.put('/users/:userId', userController.update)
+router.delete('/users/:userId', userController.delete)
+
+//Complaint routes
+router.get('/users/:userId/complaints', complaintController.index)
+router.get('/users/:userId/complaints/new', complaintController.new)
+router.get('/users/:userId/complaints', complaintController.create)
+router.get('/users/:userId/complaints/:complaintId', complaintController.show)
+router.get('/users/:userId/complaints/complaintId/edit', complaintController.edit)
+router.get('/users/:userId/complaints/complaintId', complaintController.update)
+router.get('/users/:userId/complaints/complaintId', complaintController.delete)
+
+
+
+
 
 
 module.exports = router
