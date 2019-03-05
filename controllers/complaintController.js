bee5571d390f2006b5919056aa262f5ed89879e1
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-03-05 12:57:33 -0500

    complaint routes complete

--- controllers/complaintController.js
+++ controllers/complaintController.js
@@ -1,47 +1,69 @@
-const User = require('../models/User')
-const { Complaint } = require('../models/Complaint')
+// const User = require('../models/User')
+// const Complaint = require('../models/Complaint')
 
 const complaintController = {
-    index: (req, res) => {
+    index: (req,res) => {
         res.send('hello from complaintController')
     },
-
-    new: (req, res) => {
-        res.send('Here is a blank form for a new user')
-
+    new: (req,res) => {
+        res.send('new')
     },
-    create: (req, res) => {
-        User.findById(req.params.userId)
-            .then(user => {
-                Complaint.create({
-                    content: 'Here is a new complaint',
-                    comments: [{ content: 'Yippee' }]
-                })
-                    .then(newComplaint => {
-                        user.Complaints.push(newComplaint)
-                        user.save()
-                        res.send(newComplaint)
-                    })
-            })
+    create: (req,res) => {
+        res.send('create')
     },
-    show: (req, res) => {
-        Complaint.findById(req.params.ComplaintId).then(Complaint => {
-            res.send({ Complaint })
-        })
+    show: (req,res) => {
+        res.send('show')
     },
-    edit: (req, res) => {
-        res.send(`showing form to update`)
+    edit: (req,res) => {
+        res.send('edit')
     },
-    update: (req, res) => {
-        Complaint.findByIdAndUpdate(req.params.ComplaintId, { content: 'This Complaint was updated a second time' }, { new: true }).then(updatedComplaint => {
-            res.send(updatedComplaint)
-        })
+    update: (req,res) => {
+        res.send('update')
     },
-    delete: (req, res) => {
-        Complaint.findByIdAndDelete(req.params.ComplaintId).then(() => {
-            res.send(`Deleted Complaint with Complaint if of ${req.params.ComplaintId}`)
-        })
+    delete: (req,res) => {
+        res.send('delete')
     }
+    
+    // index: (req, res) => {
+    //     res.send('hello from complaintController')
+    // },
+
+    // new: (req, res) => {
+    //     res.send('Here is a blank form for a new user')
+
+    // },
+    // create: (req, res) => {
+    //     User.findById(req.params.userId)
+    //         .then(user => {
+    //             Complaint.create({
+    //                 content: 'Here is a new complaint',
+    //                 comments: [{ content: 'Yippee' }]
+    //             })
+    //                 .then(newComplaint => {
+    //                     user.Complaints.push(newComplaint)
+    //                     user.save()
+    //                     res.send(newComplaint)
+    //                 })
+    //         })
+    // },
+    // show: (req, res) => {
+    //     Complaint.findById(req.params.ComplaintId).then(Complaint => {
+    //         res.send({ Complaint })
+    //     })
+    // },
+    // edit: (req, res) => {
+    //     res.send(`showing form to update`)
+    // },
+    // update: (req, res) => {
+    //     Complaint.findByIdAndUpdate(req.params.ComplaintId, { content: 'This Complaint was updated a second time' }, { new: true }).then(updatedComplaint => {
+    //         res.send(updatedComplaint)
+    //     })
+    // },
+    // delete: (req, res) => {
+    //     Complaint.findByIdAndDelete(req.params.ComplaintId).then(() => {
+    //         res.send(`Deleted Complaint with Complaint if of ${req.params.ComplaintId}`)
+    //     })
+    // }
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const complaintController = require('../controllers/complaintController')
 
 //User routes
 router.get('/', userController.index)
@@ -12,13 +13,13 @@ router.put('/:userId', userController.update)
 router.delete('/:userId', userController.delete)
 
 //Complaint routes
-// router.get('/users/:userId/complaints', complaintController.index)
-// router.get('/users/:userId/complaints/new', complaintController.new)
-// router.get('/users/:userId/complaints', complaintController.create)
-// router.get('/users/:userId/complaints/:complaintId', complaintController.show)
-// router.get('/users/:userId/complaints/complaintId/edit', complaintController.edit)
-// router.get('/users/:userId/complaints/complaintId', complaintController.update)
-// router.get('/users/:userId/complaints/complaintId', complaintController.delete)
+router.get('/:userId/complaints', complaintController.index)
+router.get('/:userId/complaints/new', complaintController.new)
+router.post('/:userId/complaints', complaintController.create)
+router.get('/:userId/complaints/:complaintId', complaintController.show)
+router.get('/:userId/complaints/:complaintId/edit', complaintController.edit)
+router.put('/:userId/complaints/:complaintId', complaintController.update)
+router.delete('/:userId/complaints/:complaintId', complaintController.delete)
 
 
 
