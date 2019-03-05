61a9283924bf86358a2bb3a7d30fa95ad81d1776
Peter Fitzpatrick <peterfitzpatrick@Peters-MacBook-Pro.local>
2019-03-05 12:10:19 -0500

    user routes and controllers working

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,24 @@ const userController = {
     index: (req,res) => {
         res.send('hello from usercontroller')
     },
-    
+    new: (req,res) => {
+        res.send('new')
+    },
+    create: (req,res) => {
+        res.send('create')
+    },
+    show: (req,res) => {
+        res.send('show')
+    },
+    edit: (req,res) => {
+        res.send('edit')
+    },
+    update: (req,res) => {
+        res.send('update')
+    },
+    delete: (req,res) => {
+        res.send('delete')
+    }
 }
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 //User routes
-router.get('/users', userController.index)
-// router.get('/users/new', userController.new)
-// router.post('/users', userController.create)
-// router.get('/users/:userId', userController.show)
-// router.get('/users/:userId/edit', userController.edit)
-// router.put('/users/:userId', userController.update)
-// router.delete('/users/:userId', userController.delete)
+router.get('/', userController.index)
+router.get('/new', userController.new)
+router.post('/', userController.create)
+router.get('/:userId', userController.show)
+router.get('/:userId/edit', userController.edit)
+router.put('/:userId', userController.update)
+router.delete('/:userId', userController.delete)
 
 //Complaint routes
 // router.get('/users/:userId/complaints', complaintController.index)
